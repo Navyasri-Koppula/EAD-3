67a48dfa2c28fbf085a8d415f44a7a3ce22fa8b2
Navyasri-Koppula <147251903+Navyasri-Koppula@users.noreply.github.com>
2024-09-10 15:07:14 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function appendToDisplay(value)
+{
+    document.getElementById('display').value+=value
+}
+function clearDisplay()
+{
+    document.getElementById('display').value='0'
+}
+function calculate()
+{
+    const display=document.getElementById('display')
+    try{
+        display.value=eval(display.value)
+    }
+    catch{
+        display.value='Error'
+    }
+}
